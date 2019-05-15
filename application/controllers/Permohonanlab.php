6b3c8cb368779ed05cbcc1d4694ea902fab35041
hantunya <hantunya@yahoo.com>
2019-05-15 23:22:09 +0700

    ada

--- application/controllers/Permohonanlab.php
+++ application/controllers/Permohonanlab.php
@@ -17,8 +17,8 @@ class PermohonanLab extends BaseController
     public function __construct()
     {
         parent::__construct();
-      //  $this->load->model('Permohonan_model');
-       /// $this->load->model('user_model');
+       $this->load->model('Permohonan_model');
+        $this->load->model('user_model');
         $this->db2 = $this->load->database ('my_sqlsrv', TRUE );
         $this->isLoggedIn();   
     }
